86e751a528f0de0fa91a42777dce618fd2e3da4f
OrthodoxWinter <bowenxie2006@hotmail.com>
2016-10-09 16:32:20 -0400

    comment for q1 and clean up temporary files

--- A2/q1.cc
+++ A2/q1.cc
@@ -6,6 +6,10 @@
 
 using namespace std;
 
+/*
+ * This is an example type that can be sorted using Binsertsort.
+ * It provides a == operator and < operator, as specified in the assignment
+ */
 class TwoNumbers {
   public:
     int value1;
@@ -25,65 +29,90 @@ class TwoNumbers {
     }
 };
 
+/*
+ * This function is an example usage of using BinsertSort to sort 
+ * the type TwoNumbers defined above
+ */
+void sortTwoNumbers() {
+    TwoNumbers sentinel(-1, -1);                                                        // Sentinel Value
+    Binsertsort<TwoNumbers> sort(sentinel);
+    TwoNumbers one(1, 6);                                                               // Values to sort
+    TwoNumbers two(7, 5);
+    TwoNumbers three(6, 3);
+    TwoNumbers four(6, 6);
+    sort.sort(one);
+    sort.sort(two);
+    sort.sort(three);
+    sort.sort(four);
+    sort.sort(sentinel);
+    for (int i = 0; i < 4; i++) {                                                       // retrieve and print out the sorted values
+        TwoNumbers value = sort.retrieve();
+        cout << value.value1 << " " << value.value2 << endl;
+    }
+}
+
 // print out the usage of this program and terminate
 void usage( char *argv[] ) {
     cerr << "Usage: " << argv[0]
-     << " [infile-file]" << endl;
+     << " unsorted-file [ sorted-file ]" << endl;
     exit( EXIT_FAILURE );
 }
 
 void uMain::main() {
 
-    istream *infile;                                                                    // default input is cin
-    ostream *outfile = &cout;
+    istream *infile;
+    ostream *outfile = &cout;                                                           // default output is cout
 
     switch ( argc ) {
-        case 3:                                                                         // input file provided, therefore read from file
+        case 3:                                                                         // output file specified, therefore open stream to write to file
             try {
                 outfile = new ofstream(argv[2]);
             } catch (uFile::Failure) {
-                cout << "Error! Could not create output file \"" << argv[2] << "\"" << endl;
-            }
-        case 2:
+                cout << "Error! Could not open output file \"" << argv[2] << "\"" << endl;
+                usage(argv);
+            } // try
+        case 2:                                                                         // open stream to read from input file
             try {
                 infile = new ifstream(argv[1]);
             } catch (uFile::Failure) {
                 cout << "Error! Could not open input file \"" << argv[1] << "\"" << endl;
-            }
-            break;                                                                      // no input file, use cin
+                usage(argv);
+            } // try
+            break;
         default:
-            usage(argv);                                                                // more than one argument, incorrect usage
+            usage(argv);                                                                // too many or too few arguments, show correct usage
     } // switch
 
     string line;
 
-    while (getline(*infile, line)) {
+    while (getline(*infile, line)) {                                                    // read input file one line at a time
         if (line == "") {
-            continue;
+            continue;                                                                   // move on to next line if encountered an empty line
         }
         stringstream input(line);
 
         Binsertsort<int> sort(SENTINEL);
-        int numValue;
+        int numValue;                                                                   // number of values to sort on current line
         int value;
 
         input >> numValue;
-        for (int i = 0; i < numValue; i++) {
+        for (int i = 0; i < numValue; i++) {                                            // read the numbers and sort them
             input >> value;
             *outfile << value << " ";
             sort.sort(value);
         }
+
         *outfile << endl;
 
-        sort.sort(SENTINEL);
+        sort.sort(SENTINEL);                                                            // send SENTINEL to indicate end of input
 
-        for (int i = 0; i < numValue; i++) {
+        for (int i = 0; i < numValue; i++) {                                            // retrieve and print the sorted values
             int sortedValue = sort.retrieve();
             *outfile << sortedValue << " ";
         }
         *outfile << endl << endl;
     }
 
-    delete infile;
+    delete infile;                                                                      // release resources
     if (outfile != &cout) delete outfile;
 }
--- A2/q1binsertsort.h
+++ A2/q1binsertsort.h
@@ -2,14 +2,20 @@
 #define __BINSERTSORT_H__ 1
 
 template<typename T> _Coroutine Binsertsort {
-    const T Sentinel;            // value denoting end of set
-    T value;                     // communication: value being passed down/up the tree
-    Binsertsort<T> *left;
-    Binsertsort<T> *right;
-    T nodeValue;
+    const T Sentinel;                                                       // value denoting end of set
+    T value;                                                                // communication: value being passed down/up the tree
+    Binsertsort<T> *left;                                                   // left child
+    Binsertsort<T> *right;                                                  // right child
+    T nodeValue;                                                            // node value
 
+    /*
+     * This function retrieve all the values in a child tree until 
+     * a Sentinel value is returned, at which point it returns.
+     * Every time a non-Sentinel value is encountered, this function suspends
+     * the coroutine
+     */
     void retrieveChild(Binsertsort<T> *child) {
-        child->sort(Sentinel);
+        child->sort(Sentinel);                                              //signal end of input to child
         for (;;) {
             value = child->retrieve();
             if (!(value == Sentinel)) {
@@ -23,42 +29,41 @@ template<typename T> _Coroutine Binsertsort {
     void main() {
         left = NULL;
         right = NULL;
-        nodeValue = value;
+        nodeValue = value;                                                  // set the node value on first resume
         suspend();
-        if (!(nodeValue == Sentinel)) {
-            Binsertsort<T> leftChild(Sentinel);
+        if (!(nodeValue == Sentinel)) {                                     // on second resume, check if the node value is Sentinel. If yes, we set value to Sentinel and terminate
+            Binsertsort<T> leftChild(Sentinel);                             //node value is not Sentinel, so we create left and right child, and prepare to read more values
             Binsertsort<T> rightChild(Sentinel);
             left = &leftChild;
             right = &rightChild;
-            for (;;) {
-                if (value == Sentinel) { 
+            for (;;) {                                                      // read values passed in by sort
+                if (value == Sentinel) {                                    //stop when a Sentinel value is encountered
                     break;
                 }
-                Binsertsort<T> *child = value < nodeValue? left : right;
+                Binsertsort<T> *child = value < nodeValue? left : right;    // give value to the correct child
                 child->sort(value);
                 suspend();
             }
 
+            suspend();                                                      // this suspend is for the Sentinel value
+
+            retrieveChild(left);                                            // retrieve all values of left child
+
+            value = nodeValue;                                              // retrieve value of the current node
             suspend();
 
-            retrieveChild(left);
-
-            value = nodeValue;
-            suspend();
-
-            retrieveChild(right);
+            retrieveChild(right);                                           // retrieve all values of right child
         }
-        value = Sentinel;
-        suspend();
+        value = Sentinel;                                                   // give back Sentinel to indicate the end
     }
 
   public:
     Binsertsort( T Sentinel ) : Sentinel( Sentinel ) {}
-    void sort( T value ) {       // value to be sorted
+    void sort( T value ) {                                                  // value to be sorted
         Binsertsort::value = value;
         resume();
     }
-    T retrieve() {               // retrieve sorted value
+    T retrieve() {                                                          // retrieve sorted value
         resume();
         return value;
     }
