f338ba45587b2f3c18c3ba276e7b01fd792d400c
OrthodoxWinter <bowenxie2006@hotmail.com>
2016-11-27 18:28:07 -0500

    Initial implementation for Groupoff

---
+++ A6/groupoff.cc
@@ -0,0 +1,43 @@
+#include "rng.h"
+#include "groupoff.h"
+
+Groupoff::Groupoff( Printer &prt, unsigned int numStudents, unsigned int sodaCost, unsigned int groupoffDelay ) : printer(prt), numStudents(numStudents), sodaCost(sodaCost), groupoffDelay(groupoffDelay) {
+	futureCards = new WATCard::FWATCard[numStudents];
+	nextFuture = 0;
+}
+
+Groupoff::~Groupoff() {
+	delete[] futureCards;
+}
+
+WATCard::FWATCard Groupoff::giftCard() {
+	WATCard::FWATCard future = futureCards[nextFuture];
+	nextFuture++;
+	return future;
+}
+
+void Groupoff::main() {
+	printer.print(Printer::Kind::Groupoff, 'S');
+	unsigned int numAvailableFutures = numStudents;
+	for (unsigned int i = 0; i < numStudents; i++) {
+		_Accept(giftCard);
+	}
+	for (;;) {
+		_Accept(~Groupoff) {
+			break;
+		} _Else {
+			yield(groupoffDelay);
+			unsigned int selection = rng() % numAvailableFutures;				// randomly select a future
+			WATCard::FWATCard toGift = futureCards[selection];
+			futureCards[selection] = futureCards[numAvailableFutures - 1];		// swap the future we just selected into the last position
+			futureCards[numAvailableFutures - 1] = toGift;
+			numAvailableFutures--;												// decrement numAvailableFutures, thus marking the future we just selected as unavailable
+			WATCard *card = new WATCard();										// create new card with sodaCost in it, and deliver into future
+			card->deposit(sodaCost);
+			printer.print(Printer::Kind::Groupoff, sodaCost);
+			toGift.delivery(card);
+			if (numAvailableFutures == 0) break;								// All giftcards have been delivered, so stop
+		}
+	}
+	printer.print(Printer::Kind::Groupoff, 'F');
+}
--- A6/groupoff.h
+++ A6/groupoff.h
@@ -1,11 +1,21 @@
 #ifndef __GROUPOFF_H__
 #define __GROUPOFF_H__
 
+#include "WATcard.h"
+#include "printer.h"
+
 _Task Groupoff {
+	Printer &printer;
+	unsigned int numStudents;
+	unsigned int sodaCost;
+	unsigned int groupoffDelay;
+	WATCard::FWATCard *futureCards;
+	unsigned int nextFuture;
     void main();
   public:
     Groupoff( Printer &prt, unsigned int numStudents, unsigned int sodaCost, unsigned int groupoffDelay );
     WATCard::FWATCard giftCard();
+    ~Groupoff();
 };
 
 #endif
